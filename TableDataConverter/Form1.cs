f6eb6c6df40eeeca6cc0635c448b5f2a120dff0a
RohTaeyoung <40551068+RohTaeyoung@users.noreply.github.com>
2025-11-20 19:03:31 +0900

    윈폼으로 프로젝트 변경 및 단일 파일화

---
+++ TableDataConverter/Form1.cs
@@ -0,0 +1,271 @@
+using ClosedXML.Excel;
+using System.Text;
+
+namespace TableDataConverter
+{
+    public partial class Form1 : Form
+    {
+        //
+        static public string pPathScript = string.Empty;
+        static public string pPathData = string.Empty;
+
+        //
+        ManagerTableCreater _mtCreater;
+
+        //
+        List<FileInfo> _fileInfos;
+
+        //
+        StringBuilder _sb;
+
+        public Form1()
+        {
+            //
+            InitializeComponent();
+
+            //
+            var path = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            path = $"{path}\\{new DirectoryInfo(AppContext.BaseDirectory).Name.Replace("Tables", "")}";
+            pPathScript = $"{path}\\Assets\\Scripts\\_Common\\Table";
+            pPathData = $"{path}\\Assets\\Table";
+
+            //
+            _mtCreater = new ManagerTableCreater();
+            _sb = new StringBuilder();
+
+            RefreshFileInfos();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void RefreshFileInfos()
+        {
+            //
+            if (_fileInfos == null)
+                _fileInfos = new List<FileInfo>();
+
+            _fileInfos.Clear();
+
+            //
+            var dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            foreach (var item in dirInfo.GetFiles())
+            {
+                if (item.Name[0] == '_' && item.Name.IndexOf(".xlsx") >= 0)
+                {
+                    _fileInfos.Add(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBtn_Refresh(object sender, EventArgs e)
+        {
+            RefreshFileInfos();
+
+            MessageBox.Show($"갱신");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBtn_Confirm(object sender, EventArgs e)
+        {
+            //
+            foreach (var item in _fileInfos)
+            {
+                //
+                var fileName = item.Name.Replace(".xlsx", "");
+                var loadData = new XLWorkbook(item.FullName);
+
+                CreateClass(fileName, loadData);
+                CreateData(fileName, loadData);
+            }
+
+            //
+            _mtCreater.Create(_fileInfos);
+
+            //
+            MessageBox.Show($"클릭");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="workBook"></param>
+        void CreateClass(string fileName, XLWorkbook workBook)
+        {
+            //
+            var fs = new FileStream($"{pPathScript}\\{fileName}.cs", FileMode.Create, FileAccess.Write);
+            var sw = new StreamWriter(fs);
+
+            //
+            var worksheet = workBook.Worksheet(1);
+            var range = worksheet.RangeUsed();
+
+            if (range == null)
+                return;
+
+            var tempVariables = new List<string>();
+            for (int col = 1; col <= range.ColumnCount(); col++)
+            {
+                tempVariables.Add(ClassVariable(worksheet.Cell(3, col).Value.GetText(), worksheet.Cell(2, col).Value.GetText()));
+            }
+
+            sw.Write(ClassCode(fileName, tempVariables));
+            sw.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
+        string ClassCode(string name, List<string> variables)
+        {
+            var className = name.Replace(".xlsx", "");
+
+            _sb.Clear();
+            _sb.Append($"using System;\r\nusing System.IO;\r\n\r\npublic class {className}\r\n{{");
+
+            foreach (var item in variables)
+            {
+                _sb.Append(item);
+            }
+
+            _sb.Append($"\r\n\r\n    public static {className} GetItem(int key)\r\n");
+            _sb.Append("    {\r\n");
+            _sb.Append($"        if (Manager_Table.Instance._dic{className}.ContainsKey(key))\r\n");
+            _sb.Append($"            return Manager_Table.Instance._dic{className}[key];\r\n");
+            _sb.Append("        else\r\n");
+            _sb.Append("            return null;\r\n");
+            _sb.Append("    }\r\n");
+            _sb.Append("}");
+
+            return _sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        string ClassVariable(string type, string name)
+        {
+            //
+            var init = string.Empty;
+            switch (type)
+            {
+                case "int":
+                case "long":
+                case "double":
+                    init = "0";
+                    break;
+                case "float":
+                    init = "0f";
+                    break;
+                default:
+                    return string.Empty;
+            }
+
+            //
+            _sb.Clear();
+            _sb.AppendFormat("\r\n    public {0} {1} {{ set; get; }} = {2};", type, name, init);
+
+            return _sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="workBook"></param>
+        void CreateData(string fileName, XLWorkbook workBook)
+        {
+            //
+            var fs = new FileStream($"{pPathData}\\{fileName}.bytes", FileMode.Create, FileAccess.Write);
+            var sw = new StreamWriter(fs);
+
+            //
+            var worksheet = workBook.Worksheet(1);
+            var range = worksheet.RangeUsed();
+
+            if (range == null)
+                return;
+
+            _sb.Clear();
+            _sb.Append("[");
+            var tempVariables = new List<string>();
+            for (int row = 4; row <= range.RowCount(); row++)
+            {
+                _sb.Append("{");
+                for (int col = 1; col <= range.ColumnCount(); col++)
+                {
+                    var cellValue = worksheet.Cell(row, col).Value;
+                    _sb.Append(cellValue.IsText ?
+                        DataCode(worksheet.Cell(2, col).GetText(), cellValue.GetText()) :
+                        DataCode(worksheet.Cell(2, col).GetText(), cellValue.GetNumber()));
+
+                    if (col < range.ColumnCount())
+                    {
+                        _sb.Append(",");
+                    }
+                }
+                _sb.Append("}");
+
+                if (row < range.RowCount())
+                {
+                    _sb.Append(",");
+                }
+            }
+            _sb.Append("]");
+
+
+
+            sw.Write(_sb);
+            sw.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        string DataCode(string name, string value)
+        {
+            var temp = string.Format("\"{0}\":\"{1}\"", name, value);
+
+            return temp;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        string DataCode(string name, double value)
+        {
+            var temp = string.Format("\"{0}\":{1}", name, value);
+
+            return temp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
